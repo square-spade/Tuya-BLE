9ed144d431802d32be9afa11a186654688ea9d37
square-spade <41908243+square-spade@users.noreply.github.com>
2025-09-12 22:33:34 +1000

    Update valve.py

--- custom_components/tuya_local_ble/valve.py
+++ custom_components/tuya_local_ble/valve.py
@@ -18,3 +18,141 @@ from homeassistant.core import HomeAssistant
 from homeassistant.helpers import config_validation as cv
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 from homeassistant.util import dt
+
+def get_mapping_by_device(device: TuyaBLEDevice) -> list[TuyaBLECategorySwitchMapping]:
+    category = mapping.get(device.category)
+    if category is not None and category.products is not None:
+        product_mapping = category.products.get(device.product_id)
+        if product_mapping is not None:
+            return product_mapping
+        if category.mapping is not None:
+            return category.mapping
+        else:
+            return []
+    else:
+        return []
+
+
+class TuyaBLESwitch(TuyaBLEEntity, SwitchEntity):
+    """Representation of a Tuya BLE Switch."""
+
+    def __init__(
+        self,
+        hass: HomeAssistant,
+        coordinator: DataUpdateCoordinator,
+        device: TuyaBLEDevice,
+        product: TuyaBLEProductInfo,
+        mapping: TuyaBLESwitchMapping,
+    ) -> None:
+        super().__init__(hass, coordinator, device, product, mapping.description)
+        self._mapping = mapping
+
+    @property
+    def is_on(self) -> bool:
+        """Return true if switch is on."""
+
+        if self._mapping.getter:
+            return self._mapping.getter(self, self._product)
+
+        datapoint = self._device.datapoints[self._mapping.dp_id]
+        if datapoint:
+            if (
+                datapoint.type
+                in [TuyaBLEDataPointType.DT_RAW, TuyaBLEDataPointType.DT_BITMAP]
+                and self._mapping.bitmap_mask
+            ):
+                bitmap_value = bytes(datapoint.value)
+                bitmap_mask = self._mapping.bitmap_mask
+                for v, m in zip(bitmap_value, bitmap_mask, strict=True):
+                    if (v & m) != 0:
+                        return True
+            else:
+                return bool(datapoint.value)
+        return False
+
+    def turn_on(self, **kwargs: Any) -> None:
+        """Turn the switch on."""
+        if self._mapping.setter:
+            return self._mapping.setter(self, self._product, True)
+
+        new_value: bool | bytes
+        if self._mapping.bitmap_mask:
+            datapoint = self._device.datapoints.get_or_create(
+                self._mapping.dp_id,
+                TuyaBLEDataPointType.DT_BITMAP,
+                self._mapping.bitmap_mask,
+            )
+            bitmap_mask = self._mapping.bitmap_mask
+            bitmap_value = bytes(datapoint.value)
+            new_value = bytes(
+                v | m for (v, m) in zip(bitmap_value, bitmap_mask, strict=True)
+            )
+        else:
+            datapoint = self._device.datapoints.get_or_create(
+                self._mapping.dp_id,
+                TuyaBLEDataPointType.DT_BOOL,
+                True,
+            )
+            new_value = True
+        if datapoint:
+            self._hass.create_task(datapoint.set_value(new_value))
+
+    def turn_off(self, **kwargs: Any) -> None:
+        """Turn the switch off."""
+        if self._mapping.setter:
+            return self._mapping.setter(self, self._product, False)
+
+        new_value: bool | bytes
+        if self._mapping.bitmap_mask:
+            datapoint = self._device.datapoints.get_or_create(
+                self._mapping.dp_id,
+                TuyaBLEDataPointType.DT_BITMAP,
+                self._mapping.bitmap_mask,
+            )
+            bitmap_mask = self._mapping.bitmap_mask
+            bitmap_value = bytes(datapoint.value)
+            new_value = bytes(
+                v & ~m for (v, m) in zip(bitmap_value, bitmap_mask, strict=True)
+            )
+        else:
+            datapoint = self._device.datapoints.get_or_create(
+                self._mapping.dp_id,
+                TuyaBLEDataPointType.DT_BOOL,
+                False,
+            )
+            new_value = False
+        if datapoint:
+            self._hass.create_task(datapoint.set_value(new_value))
+
+    @property
+    def available(self) -> bool:
+        """Return if entity is available."""
+        result = super().available
+        if result and self._mapping.is_available:
+            result = self._mapping.is_available(self, self._product)
+        return result
+
+
+async def async_setup_entry(
+    hass: HomeAssistant,
+    entry: ConfigEntry,
+    async_add_entities: AddEntitiesCallback,
+) -> None:
+    """Set up the Tuya BLE sensors."""
+    data: TuyaBLEData = hass.data[DOMAIN][entry.entry_id]
+    mappings = get_mapping_by_device(data.device)
+    entities: list[TuyaBLESwitch] = []
+    for mapping in mappings:
+        if mapping.force_add or data.device.datapoints.has_id(
+            mapping.dp_id, mapping.dp_type
+        ):
+            entities.append(
+                TuyaBLESwitch(
+                    hass,
+                    data.coordinator,
+                    data.device,
+                    data.product,
+                    mapping,
+                )
+            )
+    async_add_entities(entities)
