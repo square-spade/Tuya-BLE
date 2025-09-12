5df0a3091026e4f40ee00213c02a9a7a71fbfeb5
square-spade <41908243+square-spade@users.noreply.github.com>
2025-09-12 23:13:05 +1000

    Update number.py

--- custom_components/tuya_local_ble/number.py
+++ custom_components/tuya_local_ble/number.py
@@ -434,7 +434,25 @@ mapping: dict[str, TuyaBLECategoryNumberMapping] = {
             ],
         },
     ),
+    "sfkzq": TuyaBLECategoryNumberMapping(
+        products={
+            "svhikeyq": [  # Irrigation computer
+                TuyaBLENumberMapping(
+                    dp_id=11,
+                    description=NumberEntityDescription(
+                        key="countdown",
+                        icon="mdi:timer",
+                        native_max_value=86400,
+                        native_min_value=0,
+                        native_unit_of_measurement=UnitOfTime.SECONDS,
+                        native_step=1,
+                    ),
+                ),
+            ],
+        },
+    ),
 }
+
 
 
 def get_mapping_by_device(device: TuyaBLEDevice) -> list[TuyaBLECategoryNumberMapping]:
